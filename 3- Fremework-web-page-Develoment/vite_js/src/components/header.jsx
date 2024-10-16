07a94390f547635c9e9432a11fda414d2029e97f
hai-chamroeurn <haichamroeurn@gmail.com>
2024-10-16 17:02:24 +0700

    hello

--- 3- Fremework-web-page-Develoment/vite_js/src/components/header.jsx
+++ 3- Fremework-web-page-Develoment/vite_js/src/components/header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 export default function Header() {
+  var a=10;
+  let b=20;
+  const c=30;
+  function sum(a,b){
+    return a+b;
+  }
   return (
-    <div>header</div>
-  )
+    <div>
+      <h1 style={{color:'red',textAlign:'center'}}>Hello world</h1>
+      <p>Result a this {a}</p>
+      <p>Result b this {b}</p>
+      <p>Result c this {c}</p>
+    </div>
+  );
 }
